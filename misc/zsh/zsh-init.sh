ed43e6fc2caa3b4a5c677b233ba5fe02b27045ca
1xyz <58613330+1xyz@users.noreply.github.com>
2021-04-22 14:00:35 -0700

    record history from zsh shell

---
+++ misc/zsh/zsh-init.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+SCRIPT_SOURCE=$0
+AARD_DIR="$(dirname "$(realpath "$SCRIPT_SOURCE")")"
+AARD_BIN=${AARD_DIR}/aard
+
+function aard_append_history() { echo $1 | ${AARD_BIN} history append --stdin; }
+
+autoload -Uz add-zsh-hook
+add-zsh-hook zshaddhistory aard_append_history
