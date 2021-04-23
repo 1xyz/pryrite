ee8a79df3deb189762da243bdaf62d8581b6b5f4
1xyz <58613330+1xyz@users.noreply.github.com>
2021-04-23 11:25:58 -0700

    Ensure log files are trimmed

--- go.mod
+++ go.mod
@@ -13,5 +13,6 @@ require (
 	github.com/rs/zerolog v1.21.0
 	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
 	golang.org/x/term v0.0.0-20210317153231-de623e64d2a6
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
--- tools/log.go
+++ tools/log.go
@@ -1,18 +1,107 @@
 package tools
 
 import (
+	"fmt"
 	"github.com/rs/zerolog"
 	zlog "github.com/rs/zerolog/log"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"gopkg.in/yaml.v2"
 	"io"
+	"os"
+	"path/filepath"
 	"time"
 )
 
 var (
-	Log = zlog.Logger
+	Log           = zlog.Logger
+	logConfigFile = os.ExpandEnv("$HOME/.aardvark/logging.yaml")
+	logFile       = os.ExpandEnv("$HOME/.aardvark/aard.log")
 )
 
-func InitLogger(w io.Writer, level zerolog.Level) {
+type rollingLogConfig struct {
+	MaxSizeMB  int    `yaml:"max_size_mb"`
+	MaxBackups int    `yaml:"max_backups"`
+	MaxAgeDays int    `yaml:"max_age_days"`
+	Filename   string `yaml:"filename"`
+}
+
+// createLogConfigFile creates a default rolling log config file
+// if ones does not exists
+func createLogConfigFile() error {
+	exists, err := StatExists(logConfigFile)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return nil
+	}
+
+	c := &rollingLogConfig{
+		MaxSizeMB:  5,
+		MaxBackups: 2,
+		MaxAgeDays: 30,
+		Filename:   logFile,
+	}
+
+	fp, err := OpenFile(logConfigFile, os.O_WRONLY|os.O_CREATE|os.O_TRUNC)
+	if err != nil {
+		return fmt.Errorf("OpenFile %s err = %v", logConfigFile, err)
+	}
+	defer CloseFile(fp)
+	enc := yaml.NewEncoder(fp)
+	return enc.Encode(&c)
+}
+
+// loadRollingLogConfig loads a rolling log configuration from file
+func loadRollingLogConfig() (*rollingLogConfig, error) {
+	if err := createLogConfigFile(); err != nil {
+		return nil, err
+	}
+
+	fp, err := OpenFile(logConfigFile, os.O_RDONLY)
+	if err != nil {
+		return nil, err
+	}
+
+	dec := yaml.NewDecoder(fp)
+	c := rollingLogConfig{}
+	if err := dec.Decode(&c); err != nil {
+		return nil, err
+	}
+	return &c, nil
+}
+
+// OpenLogger opens the default rolling file logger
+// and sets the verbosity of the logger
+func OpenLogger(verbose bool) (io.Closer, error) {
+	// create a log config if needed
+	if err := createLogConfigFile(); err != nil {
+		return nil, err
+	}
+	// load the config from file
+	c, err := loadRollingLogConfig()
+	if err != nil {
+		return nil, err
+	}
+	// ensure that the log directory is present
+	if err := EnsureDir(filepath.Dir(c.Filename)); err != nil {
+		return nil, err
+	}
+	// create log writer
+	w := &lumberjack.Logger{
+		Filename:   c.Filename,
+		MaxBackups: c.MaxBackups,
+		MaxSize:    c.MaxSizeMB,
+		MaxAge:     c.MaxAgeDays,
+	}
+	// set log level
+	level := zerolog.InfoLevel
+	if verbose {
+		level = zerolog.DebugLevel
+	}
+	// initialize logger
 	Log = zlog.Output(w).Level(level)
+	return w, nil
 }
 
 func TrimLength(s string, maxLen int) string {
